1c0448cd3f2619b511d2ae85f04bf3a5cfe2dd16
dest-windows11-victus <dest-windows11@victus>
2024-05-20 16:44:00 +0530

    changed order

--- src/renderer/components/pages/AllStudents.jsx
+++ src/renderer/components/pages/AllStudents.jsx
@@ -38,56 +38,56 @@ const AllStudents = () => {
 			<TableHeader>
 				<TableRow>
 					<TableHead className="w-[100px]">Student ID</TableHead>
-					<TableHead>Aadhar No</TableHead>
-					<TableHead>Name</TableHead>
-					<TableHead>Surname</TableHead>
-					<TableHead>Father's Name</TableHead>
-					<TableHead>Mother's Name</TableHead>
-					<TableHead>Religion</TableHead>
-					<TableHead>Caste</TableHead>
-					<TableHead>Sub-Caste</TableHead>
-					<TableHead>Place of Birth</TableHead>
-					<TableHead>Taluka</TableHead>
-					<TableHead>District</TableHead>
-					<TableHead>State</TableHead>
-					<TableHead>Date of Birth</TableHead>
-					<TableHead>Last Attended School</TableHead>
-					<TableHead>Last Attended School Standard</TableHead>
-					<TableHead>Date of Admission</TableHead>
-					<TableHead>Admission Standard</TableHead>
-					<TableHead>Progress</TableHead>
-					<TableHead>Conduct</TableHead>
-					<TableHead>Date of Leaving</TableHead>
-					<TableHead>Current Standard</TableHead>
-					<TableHead>Reason of Leaving</TableHead>
-					<TableHead>Remarks</TableHead>
+					<TableHead className="w-[200px]">Name</TableHead>
+					<TableHead className="w-[150px]">Father's Name</TableHead>
+					<TableHead className="w-[150px]">Mother's Name</TableHead>
+					<TableHead className="w-[150px]">Aadhar No</TableHead>
+					<TableHead className="w-[150px]">Date of Birth</TableHead>
+					<TableHead className="w-[150px]">Place of Birth</TableHead>
+					<TableHead className="w-[200px]">Religion</TableHead>
+					<TableHead className="w-[150px]">Caste</TableHead>
+					<TableHead className="w-[150px]">Sub-Caste</TableHead>
+					<TableHead className="w-[150px]">Taluka</TableHead>
+					<TableHead className="w-[150px]">District</TableHead>
+					<TableHead className="w-[100px]">State</TableHead>
+					<TableHead className="w-[200px]">Last Attended School</TableHead>
+					<TableHead className="w-[150px]">Last School Standard</TableHead>
+					<TableHead className="w-[150px]">Date of Admission</TableHead>
+					<TableHead className="w-[200px]">Admission Standard</TableHead>
+					<TableHead className="w-[200px]">Current Standard</TableHead>
+					<TableHead className="w-[150px]">Progress</TableHead>
+					<TableHead className="w-[150px]">Conduct</TableHead>
+					<TableHead className="w-[150px]">Date of Leaving</TableHead>
+					<TableHead className="w-[200px]">Reason of Leaving</TableHead>
+					<TableHead className="w-[200px]">Remarks</TableHead>
 				</TableRow>
 			</TableHeader>
 			<TableBody>
 				{students.map((student) => (
 					<TableRow key={student.studentId}>
 						<TableCell className="font-medium">{student.studentId}</TableCell>
-						<TableCell>{student.aadharNo}</TableCell>
-						<TableCell>{student.name}</TableCell>
-						<TableCell>{student.surname}</TableCell>
+						<TableCell>
+							{student.name} {student.surname}
+						</TableCell>
 						<TableCell>{student.fathersName}</TableCell>
 						<TableCell>{student.mothersName}</TableCell>
+						<TableCell>{student.aadharNo}</TableCell>
+						<TableCell>{student.dob}</TableCell>
+						<TableCell>{student.placeOfBirth}</TableCell>
 						<TableCell>{student.religion}</TableCell>
 						<TableCell>{student.caste}</TableCell>
 						<TableCell>{student.subCaste}</TableCell>
-						<TableCell>{student.placeOfBirth}</TableCell>
 						<TableCell>{student.taluka}</TableCell>
 						<TableCell>{student.district}</TableCell>
 						<TableCell>{student.state}</TableCell>
-						<TableCell>{student.dob}</TableCell>
 						<TableCell>{student.lastAttendedSchool}</TableCell>
 						<TableCell>{student.lastSchoolStandard}</TableCell>
 						<TableCell>{student.dateOfAdmission}</TableCell>
 						<TableCell>{student.admissionStandard}</TableCell>
+						<TableCell>{student.currentStandard}</TableCell>
 						<TableCell>{student.progress}</TableCell>
 						<TableCell>{student.conduct}</TableCell>
 						<TableCell>{student.dateOfLeaving}</TableCell>
-						<TableCell>{student.currentStandard}</TableCell>
 						<TableCell>{student.reasonOfLeaving}</TableCell>
 						<TableCell>{student.remarks}</TableCell>
 					</TableRow>
